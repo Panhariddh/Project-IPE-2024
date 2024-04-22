fd92040a1b377d9798b85dc26a7da9da2405e8ec
CHHUEN CHIHAY <hay.mario168@gmail.com>
2024-04-22 12:50:06 +0700

    JS file

---
+++ index.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.eather-details');
+const error404 = document.querySelector('.not-found');
